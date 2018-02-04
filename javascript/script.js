643f9ba149d889491f9f5072832c72a429c43aa5
happymusic <supra3946@gmail.com>
2018-02-04 21:10:55 +0100

    projet web

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+
+function randomValue() {
+    var random = Math.floor(Math.random() * 10) + 1;
+    console.log(random);
+    return random;
+}
+var random = randomValue();
+
+function checkResult(e) {
+    var number = document.getElementById('number').value;
+    if (number > random) {
+        alert("Trop grand :( ");
+        return false;
+    }
+    else if (number < random) {
+        alert("Trop petit :( )");
+        return false;
+    }
+    else {
+        alert("Bravo ! \(^_^)/" );
+        var restart = confirm("Voulez-vous rejouer une autre partie ?");
+        if (restart)
+        {
+            random = randomValue();
+        }
+    }
+}
